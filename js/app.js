00fb28937614fc7c93ad60210f136b123c5fdf17
superwoods <st_sister@icloud.com>
2016-02-11 14:59:15 +0800

    临时备份

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 ﻿// @St. 2016-01-27-17.14
+// init controller
+var controller = new ScrollMagic.Controller();
+
 // 当页面加载完毕时开始动画。
 window.onload = function() {
-  //animateLogo();
-  //alert('aaaa')
   ani.init();
   updateSliderControl();
   addSmoothScrolling();
@@ -13,17 +14,6 @@ window.onscroll = function() {
   updateSliderControl();
   //locator.stop();
 }
-
-//hash监听
-/*var locator = {
-    start: function (handler) {
-            //window.onhashchange = handler;
-            //handler();
-        },
-        stop: function () {
-            window.onhashchange = null;
-        }
-};*/
 
 var ani = {
     init: function (){
@@ -41,13 +31,13 @@ var ani = {
             css: {
               y: "30px",
             },
-            
+
             // 永久重复动画的选项
             repeat: -1,
-            
+
             // 反转、重新运行动画的选项
             yoyo: true,
-            
+
             // 改变 easing 类型
             ease: Sine.easeInOut
             }
@@ -65,15 +55,23 @@ var ani = {
 function updateSliderControl() {
   // 获得所有的 slider 链接
     var links = document.querySelectorAll("#slider-control a");
-    
+
     for(var i = 0; i < links.length; i++) {
         var link = links[i];
+
+        //console.log(link);
+
         var attr = link.getAttribute('href');
-        
-        //console.log(attr);
+
+
+
         // 获取被链接指向的部分
         //var section = document.querySelector('#intro-section', '#native', '#touch', '#android');
         var section = document.querySelector(attr);
+
+        //console.log(section);
+
+
         var sectionTop    = section.offsetTop;
         //var sectionBottom = sectionTop + window.innerHeight;  //  section.offsetHeight
         var sectionBottom = sectionTop + section.offsetHeight;
@@ -81,13 +79,14 @@ function updateSliderControl() {
         //console.log(sectionTop + " / " + sectionBottom);
         //console.log(window.scrollY);
         //console.log(window.scrollY);
-        
+
         // 检查 window.scrollY 是否在这部分中
         if(window.scrollY >= sectionTop && window.scrollY < sectionBottom) {
             //console.log(attr);
             link.className = "active";
             //event.preventDefault();
             //location.hash = attr;
+            //console.log(link);
         }
         else {
             link.className = "";
@@ -97,7 +96,18 @@ function updateSliderControl() {
 
 
 // 练习：网页滚动动画
-function scrollToElement(element) {
+function scrollToElement(element, num) {
+  // create a scene
+  // var scene1 = new ScrollMagic.Scene({
+  //         duration: 100,  // the scene should last for a scroll distance of 100px
+  //         offset: 50      // start this scene after scrolling for 50px
+  //     })
+  //     .setPin("#my-sticky-element") // pins the element for the the scene's duration
+  //     .addTo(controller); // assign the scene to the controller
+
+  //console.log(num);
+  //console.log(element);
+
   //声明变量topOfElement = element.offsetTop
   var topOfElement = element.offsetTop;
   // window 的动画滚动，使用TweenMax plugins
@@ -111,17 +121,17 @@ function scrollToElement(element) {
 
 function addSmoothScrolling() {
     var links = document.querySelectorAll("#slider-control a");
-    
+
     for(var i = 0; i < links.length; i++) {
         var link = links[i];
-        
+
         //if (typeof window.addEventListener === 'function'){
         // 闭包
-        (function (_link) {
+        (function (_link, num) {
             //console.log('_link: ' + _link);
             //console.log(link);
             link.addEventListener('click', function(event){
-                /* 
+                /*
                   这里禁用了鼠标的点击事件, 会导致hash无法更新，
                   也就是说hash就没有作用了
                   动画是否可以考虑换一种逻辑方式，
@@ -130,25 +140,9 @@ function addSmoothScrolling() {
                 event.preventDefault();
                 var attr = _link.getAttribute('href');
                 //console.log('href: ' + _link);
-                scrollToElement(document.querySelector(attr));
+                scrollToElement(document.querySelector(attr), num);
                 //location.hash = attr;
             });
-        })(link);
-        //}
-        //link.addEventListener("click", function(event) {
-//        // `event` 是鼠标点击事件
-//        //event.preventDefault();
-//        
-//        // BUG 警告！使用闭包或者 ES6 `let` 修复。
-//        //var href = link.blahblahblah;
-//            (function( _p ){
-//                link.addEventListener("click", function(event) {
-//                    event.preventDefault() ;
-//                    scrollToElement(_p) ;
-//                    return false;
-//                });
-//            //scrollToElement(...);
-//        });
+        })(link, i);
     }
 }
-
